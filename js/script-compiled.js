6a53cee261406c4d191b23b69507c70678dc47f7
Rachel Miller <millerrach@gmail.com>
2016-10-31 11:43:50 +0000

    Added Loading Intro

--- js/script-compiled.js
+++ js/script-compiled.js
@@ -1,3 +1,7 @@
-"use strict";
+'use strict';
+
+TweenMax.to('#loading', 8, { width: "100%" });
+
+TweenLite.to('#happyHere', 2, { opacity: 1 });
 
 //# sourceMappingURL=script-compiled.js.map
---
+++ js/script.js
@@ -0,0 +1,3 @@
+TweenMax.to('#loading',8, {width: "100%"});
+
+TweenLite.to('#happyHere', 2, {opacity:1});
